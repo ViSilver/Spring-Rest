f7da2758d88bda72b89652318e820936974253d0
vturcanu <victor.turcanu@endava.com>
2017-01-18 15:50:16 +0200

    Clean the tests

--- src/test/java/com/example/springtestexample/repository/AddressRepositoryTest.java
+++ src/test/java/com/example/springtestexample/repository/AddressRepositoryTest.java
@@ -9,15 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import java.util.List;
 
